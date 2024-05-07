93fe35b5ace649b42dc524c939d4f91fb46ca920
Sumukh <sumukh9036@gmail.com>
2024-05-07 13:01:27 +0530

    Added fontfamily Lato

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,11 @@
 const Hero = () => {
   return (
-    <div className="">Hero
-    
+    <div className="">
+      <div className="text-3xl text-[#FF5C5C]  font-bold ">
+        Greetings, Change Agent 
+
+      </div>
+      <h1 className="text-3xl text-rose-400 font-bold"> Greetings, Change Agent </h1>
     </div>
   )
 }
