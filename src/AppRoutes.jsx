956da2b2686da1bcdf1ff7aaa3cfbfc6eb30152b
ADITYA SINGH SIKARWAR <bs151439@gmail.com>
2024-05-08 16:24:02 +0530

    login register page added

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -3,6 +3,7 @@ import {Routes, Route, Navigate} from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import Fundraisers from "./pages/Fundraisers"
 import Navbar from "./components/Navbar"
+import AuthPage from "./pages/AuthPage"
 
 
 const AppRoutes = () => {
@@ -12,6 +13,7 @@ const AppRoutes = () => {
     <Routes>
         <Route path="/" element={  <HomePage/> } ></Route>
         <Route path="/fundraisers" element={<Fundraisers/> } ></Route>
+        <Route path='/auth' element={<AuthPage/>} />
         
 
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import {BrowserRouter as Router} from 'react-router-dom'
 
 import './assets/styles/css/index.css'
 import AppRoutes from './AppRoutes.jsx'
+import { RecoilRoot } from 'recoil'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RecoilRoot>
     <Router>
     <AppRoutes/>
     </Router>
+    </RecoilRoot>
   </React.StrictMode>,
 )
